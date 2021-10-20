00176539e7b74b61d588027e024e0cd16dd670d2
KaHTuK <88055736+KaHTuK@users.noreply.github.com>
2021-10-21 00:29:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+  $(".menu-btn").on("click", function () {
+    $(".header_navbar-list").toggleClass("active");
+  });
+});
+
+const schemeSvg = document.querySelector(".scheme_svg");
+const totalPriceTag = document.querySelector(".price_total");
+let cost = 400;
+let totalPrice = 0;
+schemeSvg.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("booked")) {
+    event.target.classList.toggle("active");
+    let totalSeats = schemeSvg.querySelectorAll(".active").length;
+    totalPrice = totalSeats * cost;
+    totalPriceTag.textContent = totalPrice;
+  }
+});
